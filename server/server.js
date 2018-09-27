a017d7d1a4ad195827eb1a5d900a39319af67843
AK354349 <akilandeswari.dhandapani@wipro.com>
2018-09-27 18:07:28 +0530

    modified server file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
     , app = express()
     , server = require('http').Server(app);
+const PORT= process.env.PORT||8003;
 
 app.use(express.static('./../'));
 
-server.listen(8003, function() {
+server.listen(PORT, function() {
     console.log('server started on  8003');
 });
